766bbc4f4ca6f866c92357cac41cf02fd2a332d5
arifulsajib <allaboutsajib@gmail.com>
2023-02-19 21:24:40 +0600

    all done

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("scroll", function () {
+  const toastLiveExample = document.getElementById("liveToast");
+  const toast = new bootstrap.Toast(toastLiveExample);
+
+  toast.show();
+});
